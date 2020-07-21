c6c8b44639ca699894f26f9206e1e659be74ee1d
zbrachinara <zbrachinara@gmail.com>
2020-07-21 09:42:55 -0400

    Changed exceptions

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,6 @@ import templator.Templator;
 import data.ActionTask;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -13,19 +12,17 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.HashMap;
 
 public class Main extends Application {
 
     public static String outDir = "out/production/Scheduler/";
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws IOException, ParseException, org.json.simple.parser.ParseException {
         Templator actionTemplate = new Templator(new File(outDir + "data/ActionFXML_1.2.7.11.modt"));
         ActionTask testActionTask = new ActionTask(-1, actionTemplate);
 
-        testActionTask.getActionDataJSON().put("actionTitle", "TestTitle");
-
+        testActionTask.getActionData().put("actionTitle", "TestTitle");
 
         Parent root = testActionTask.load();
         primaryStage.initStyle(StageStyle.UNDECORATED);
@@ -35,11 +32,9 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) {
 
          launch(args);
-
-        // testing methods
 
     }
 
